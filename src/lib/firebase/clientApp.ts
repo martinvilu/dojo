84f959d33c5291fc8bc8ea3002e297294229a61b
Martin Rene <martinvilu@gmail.com>
2026-07-16 08:51:24 -0300

    fix(firebase): establecer api key de produccion como fallback en clientApp para evitar errores en localhost

--- src/lib/firebase/clientApp.ts
+++ src/lib/firebase/clientApp.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   projectId: "jutsu-classroom-mrtin",
   appId: "1:913557328690:web:831831bbd35cd384a67d2b",
   storageBucket: "jutsu-classroom-mrtin.firebasestorage.app",
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyFakeKeyForBuildPrerendering_NoCrash",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyALpBmkwjOAH4AaUW0sLS_8suZNns5eu4c",
   authDomain: "jutsu-classroom-mrtin.firebaseapp.com",
   messagingSenderId: "913557328690",
 };
